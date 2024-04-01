0818cf2ff5086f4fde5c142c9cb0a235cf1614b1
sivakajan-tech <my.otherwrks@gmail.com>
2024-04-01 17:07:40 +0530

    Add dynamic ip map

--- Cluster/client.py
+++ Cluster/client.py
@@ -1,4 +1,16 @@
 import socket
+from typing import List
+
+
+def get_ip_port(file_name) -> List[str:str]:
+    ip_map = {}
+    with open(file_name, "r") as file:
+        # Read lines one by one
+        for line in file:
+            # Split the line into IP address and port number
+            ip, port = line.strip().split()
+            ip_map[ip] = port
+    return ip_map
 
 
 def send_file(client_socket, file_path):
@@ -10,30 +22,31 @@ def send_file(client_socket, file_path):
 
 
 def main():
-    # Create a socket object
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Define the server address and port
-    client_address = ("0.0.0.0", 65432)
-
-    # Bind the socket to the server address and port
-    client_socket.connect(client_address)
-
-    # Listen for incoming connections
-    print("Server is listening on {}:{}".format(*client_address))
+    ip_map = get_ip_port("client_map.txt")
 
     # Prompt the client for the file path
-    file_path = input('Enter the file path: ')
+    file_path = input("Enter the file path: ")
+    file_name = file_path.split("/")[-1]
 
-    file_name = file_path.split('/')[-1]
-    client_socket.send(file_name.encode("utf-8"))
+    for ip, port in ip_map.items():
+        try:
+            with socket.socket(
+                socket.AF_INET, socket.SOCK_STREAM
+            ) as client_socket:
+                client_socket.connect((ip, int(port)))
 
-    # Send the file to the client
-    send_file(client_socket, file_path)
-    print("File sent successfully")
+                # Listen for incoming connections
+                print("Server is listening on {}:{}".format(ip, port))
 
-    # Close the client connection
-    client_socket.close()
+                client_socket.send(file_name.encode("utf-8"))
+
+                send_file(client_socket, file_path)
+                print("File sent successfully")
+
+                # Close the client connection
+                client_socket.close()
+        except Exception as e:
+            print(f"Error sending chunk to {ip}:{port}: {e}")
 
 
 if __name__ == "__main__":
--- Cluster/server.py
+++ Cluster/server.py
@@ -1,11 +1,37 @@
 import os
-import logging
 import socket
+from typing import List
+import unicodedata
 
 
-def save_file(filename, data):
-    with open(filename, "wb") as file:
-        file.write(data)
+def get_file_name(file_name_bytes) -> str:
+    """
+    This code attempts to decode the received byte stream using UTF-8 and,
+    if it fails, it falls back to decoding using `latin-1` encoding
+    and removes control characters (which may cause issues during decoding).
+    """
+    # Decode using UTF-8 and handle errors
+    try:
+        return file_name_bytes.decode("utf-8")
+    except UnicodeDecodeError:
+        # Replace non-UTF-8 characters with a placeholder or remove them
+        sanitized_file_name = "".join(
+            c
+            for c in file_name_bytes.decode("latin-1")
+            if unicodedata.category(c)[0] != "C"
+        )
+        return sanitized_file_name
+
+
+def get_ip_port(file_name) -> List[str:str]:
+    ip_map = {}
+    with open(file_name, "r") as file:
+        # Read lines one by one
+        for line in file:
+            # Split the line into IP address and port number
+            ip, port = line.strip().split()
+            ip_map[ip] = port
+    return ip_map
 
 
 def start_server(server_address):
@@ -17,12 +43,14 @@ def start_server(server_address):
     while True:
         print("Waiting for a connection...")
         client_socket, client_address = server_socket.accept()
-        print(f"Connection established from {client_address[0]}:{client_address[1]}")  # noqa E501
+        print(
+            f"Connection established from {client_address[0]}:{client_address[1]}"  # noqa E501
+        )
 
         # Receive the file name
-        file_name = client_socket.recv(1024).decode("utf-8")
+        file_name = get_file_name(client_socket.recv(1024))
         if file_name:
-            file_loc = f'./collection/{file_name}'
+            file_loc = f"./collection/{file_name}"
             os.makedirs(os.path.dirname(file_loc), exist_ok=True)
 
             # Receive the file data and save it
@@ -37,9 +65,13 @@ def start_server(server_address):
 
 
 if __name__ == "__main__":
+    ip_map = get_ip_port("client_map.txt")
+
     # Define the server address and port
-    server_address = ("0.0.0.0", 65432)
+    server_address = (
+        list(ip_map.keys())[0],
+        int(ip_map[list(ip_map.keys())[0]]),
+    )
     print("Server started successfully!\n")
-    logging.info("Server started successfully!")
 
     start_server(server_address)
