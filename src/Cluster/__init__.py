4ad89d7a11e79dcdb8997ded572c1505f25ccfb3
sivakajan-tech <my.otherwrks@gmail.com>
2024-06-29 09:33:02 +0530

    fix: Change the workflow of server file

--- src/Cluster/__init__.py
+++ src/Cluster/__init__.py
@@ -1,1 +1,1 @@
-from . import client, server # noqa F401
+from . import client, server  # noqa F401
--- src/Cluster/client.py
+++ src/Cluster/client.py
@@ -8,7 +8,9 @@ from src.MakeChunks.fileMake import combine_chunks
 
 
 def generate_random_folder_name(length=8):
-    return "".join(random.choices(string.ascii_letters + string.digits, k=length))
+    return "".join(
+        random.choices(string.ascii_letters + string.digits, k=length)
+    )
 
 
 def get_ip_port(file_name) -> List[str:str]:
@@ -21,16 +23,27 @@ def get_ip_port(file_name) -> List[str:str]:
             ip_map[ip] = port
     return ip_map
 
-def send_chunks_to_ip(chunk, ip, port):
+
+def send_chunks_to_ip(chunk, chunk_id, folder_name, ip, port):
+    chunk_name = f"{folder_name}/Chunk_{chunk_id}"
     try:
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
+        with socket.socket(
+            socket.AF_INET, socket.SOCK_STREAM
+        ) as client_socket:
             client_socket.connect((ip, int(port)))
 
             # Listen for incoming connections
             print("Server is listening on {}:{}".format(ip, port))
 
-            client_socket.sendall(chunk)
-            print("File sent successfully")
+            # Send chunk name length and chunk name
+            chunk_name_encoded = chunk_name.encode("utf-8")
+            client_socket.sendall(
+                len(chunk_name_encoded).to_bytes(4, byteorder="big")
+            )
+            client_socket.sendall(chunk_name_encoded)
+
+            client_socket.sendall(chunk)  # Send chunk data
+            print(f"Chunk_{chunk_id} sent to {ip}:{port}")
 
             # Close the client connection
             client_socket.close()
@@ -43,10 +56,7 @@ def send_chunks(folder_name, num_chunks, ip, port):
         chunk_file_path = f"{folder_name}/chunk{i}.chunk"
         with open(chunk_file_path, "rb") as chunk_file:
             chunk = chunk_file.read()
-            send_chunks_to_ip(chunk, ip, port)
-            print(f"Chunk {i} sent to {ip}:{port}")
-
-
+            send_chunks_to_ip(chunk, i, folder_name, ip, port)
 
 
 if __name__ == "__main__":
@@ -65,8 +75,8 @@ if __name__ == "__main__":
     num_chunks = make_chunks(file_path, CHUNK_SIZE, folder_name)
 
     # Step 2: Send the chunks to different IPs
-    send_chunks(folder_name,num_chunks, ip, port)
+    send_chunks(folder_name, num_chunks, ip, port)
 
     # Step 3: Combine chunks back into a single file
-    OUTPUT_FILE = '/Users/sivakajan/Semester 8/CS4262 - Distributed Systems/secure-distributed-file-storage-system/try_combined.txt'
+    OUTPUT_FILE = "/Users/sivakajan/Semester 8/CS4262 - Distributed Systems/secure-distributed-file-storage-system/try_combined.txt"  # noqa E501
     combine_chunks(folder_name, OUTPUT_FILE)
